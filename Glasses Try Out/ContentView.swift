7931a4c826f2f7dcd960024a9542b6794726c8af
Sergey Mityanin <s.mityanin@gmail.com>
2021-03-26 17:39:10 +0300

    Initial Commit

---
+++ Glasses Try Out/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Glasses Try Out
+//
+//  Created by Сергей on 26.03.2021.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
